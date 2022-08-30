9e8351e466005df72f5bc2196d48c90e8ed3b2c7
Chris Lundquist <clundquist@stripe.com>
2022-08-30 16:08:50 -0700

    well...here we are

--- lib/einhorn/safe_yaml.rb
+++ lib/einhorn/safe_yaml.rb
@@ -6,11 +6,11 @@ module Einhorn
       YAML.safe_load("---", permitted_classes: [])
     rescue ArgumentError
       def self.load(payload)
-        YAML.safe_load(payload, [Set, Symbol, Time], [], true)
+        YAML.safe_load(payload, [Set, Symbol, Time, Einhorn::Bind::Inet, Einhorn::Bind::Unix], [], true)
       end
     else
       def self.load(payload) # rubocop:disable Lint/DuplicateMethods
-        YAML.safe_load(payload, permitted_classes: [Set, Symbol, Time], aliases: true)
+        YAML.safe_load(payload, permitted_classes: [Set, Symbol, Time, Einhorn::Bind::Inet, Einhorn::Bind::Unix], aliases: true)
       end
     end
   end
